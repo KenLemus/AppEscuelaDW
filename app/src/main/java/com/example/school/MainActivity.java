0704e68dad0c4b321daeca07271bbcf09a1bbdd3
kennedy <lemuskennedy@gmail.com>
2020-10-26 22:42:51 -0600

    MI SEGUNDO COMMIT

--- app/src/main/java/com/example/school/MainActivity.java
+++ app/src/main/java/com/example/school/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.school;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+        public void onClick(View view){
+
+            Intent miIntent= null;
+
+            switch (view.getId()){
+                case R.id.img_alumno:
+                    miIntent= new Intent(MainActivity.this, Alumnos.class);
+                    break;
+                case R.id.img_grado:
+                    miIntent= new Intent(MainActivity.this, Grado.class);
+                    break;
+                case R.id.img_inscripcion:
+                    miIntent= new Intent(MainActivity.this, Inscripcion.class);
+                    break;
+                case R.id.img_mostrar:
+                    miIntent= new Intent(MainActivity.this, Mostrar.class);
+
+            }startActivity(miIntent);
+        }
 }
